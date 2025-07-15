1b993c34af552d145fe35133edf3cee1fa7b700a
JohnnyAppAkademie <jonathan.gur@protonmail.com>
2025-07-15 12:05:18 +0200

    Counter_Card ist nun Stateless und Main_Screen Stateful. Funktionen werden vom Main-Screen auf die Counter-Card übergeben.

--- lib/counter_card.dart
+++ lib/counter_card.dart
@@ -1,34 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CounterCard extends StatefulWidget {
-  CounterCard({super.key});
+class CounterCard extends StatelessWidget {
+  final int counter;
+  final VoidCallback onIncrement;
 
-  @override
-  _CounterCardState createState() => _CounterCardState();
-}
-
-class _CounterCardState extends State<CounterCard> {
-  int counter = 0;
+  const CounterCard({
+    super.key,
+    required this.counter,
+    required this.onIncrement,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text('Zähler: $counter'),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-              child: const Text('Click Me'),
-            ),
-          ],
-        ),
-      ),
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text('Zähler: $counter'),
+              ElevatedButton(
+                  onPressed: onIncrement, child: const Text("Zähl hoch (+1)"))
+            ],
+          )),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:stful_task/counter_card.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<int> counters = List.filled(5, 0);
+
+  void increment(int index) {
+    setState(() {
+      counters[index]++;
+    });
+  }
+
+// Sum = ( sum starts at 0, and number gets added ( a + b ))
+  int get sum => counters.fold(0, (a, b) => a + b);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          spacing: 16,
-          children: [
-            CounterCard(),
-            CounterCard(),
-            CounterCard(),
-            CounterCard(),
-            CounterCard(),
-          ],
-        ),
+        body: Center(
+      child: Column(
+        spacing: 16,
+        children: [
+          for (int i = 0; i < counters.length; i++)
+            CounterCard(
+              counter: counters[i],
+              onIncrement: () => increment(i),
+            ),
+          const SizedBox(height: 15),
+          Text('Summe: $sum', style: const TextStyle(fontSize: 24)),
+        ],
       ),
-    );
+    ));
   }
 }
